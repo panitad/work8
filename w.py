6101421098617fe0ff8cd87c6afaeeed23bf1e06
panitad <139569094+panitad@users.noreply.github.com>
2024-02-24 23:33:56 -0800

    Create w.py

---
+++ w.py
@@ -0,0 +1,24 @@
+n = 1
+while n <= 21:
+    print(n, end=', ')
+    n += 2
+print()
+n = 2
+while n <= 17:
+    print(n, end=', ')
+    n += 3
+print()
+n = 100
+while n >= -100:
+    print(n, end=', ')
+    n -= 10
+print()
+n = -30
+while n <= 30:
+    print(n, end=', ')
+    n += 10
+print()
+n = 15
+while n <= 55:
+    print(n, end=', ')
+    n += 8
